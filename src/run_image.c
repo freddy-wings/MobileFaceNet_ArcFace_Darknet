d9235505dd3ce488503cb8e5861c16a2eedcda51
louishsu <is.louishsu@foxmail.com>
2019-05-25 11:59:06 +0800

    mobilefacenet

--- src/run_image.c
+++ src/run_image.c
@@ -29,15 +29,17 @@ int verify_input_images(int argc, char** argv)
     detect* dets = calloc(0, sizeof(dets)); int n = 0;
     detect_image(pnet, rnet, onet, im1, &n, &dets, p);
     // show_detect(im1, dets, n, "detect1", 500, 1, 1, 1);
-    if (n == 0){printf("image 1 not detected!\n"); return -1;}
-    idx = keep_one(dets, n, im1); bbox box1 = dets[idx].bx; landmark landmark1 = dets[idx].mk;
+    if (n == 0){printf("image 1 is not detected!\n"); return -1;}
+    idx = keep_one(dets, n, im1); 
+    bbox box1 = dets[idx].bx; landmark landmark1 = dets[idx].mk;
 
     // detect image2
     dets = realloc(dets, 0); n = 0;
     detect_image(pnet, rnet, onet, im2, &n, &dets, p);
     // show_detect(im2, dets, n, "detect2", 500, 1, 1, 1);
-    if (n == 0){printf("image 2 not detected!\n"); return -1;}
-    idx = keep_one(dets, n, im2); bbox box2 = dets[idx].bx; landmark landmark2 = dets[idx].mk;
+    if (n == 0){printf("image 2 is not detected!\n"); return -1;}
+    idx = keep_one(dets, n, im2); 
+    bbox box2 = dets[idx].bx; landmark landmark2 = dets[idx].mk;
     printf("OK\n");
 
     // ==================== MOBILEFACENET STEP ==================== //
@@ -46,7 +48,7 @@ int verify_input_images(int argc, char** argv)
     image crop2 = crop_image_by_box(im2, box2, H, W);
     image warped1 = align_image_with_landmark(crop1, landmark1, aligned);
     image warped2 = align_image_with_landmark(crop2, landmark2, aligned);
-    show_image(crop1, "crop1", 500); show_image(crop2, "crop2", 500);
+    // show_image(crop1, "crop1", 500); show_image(crop2, "crop2", 500);
     show_image(warped1, "warped1", 500); show_image(warped2, "warped2", 500);
     printf("OK\n");
 
--- src/run_lfw.c
+++ src/run_lfw.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+ #include <unistd.h>
 #include "darknet.h"
 #include "utils.h"
 #include "list.h"
@@ -23,23 +24,13 @@ list* get_lfw_pairs()
          *      positive: Abel_Pacheco	1	4
          *      negative: Abdel_Madi_Shabneh	1	Dean_Barker	1
         */
-        char* ptr = pair; char* s; int n = 0;
+        char* ptr = pair; char* s;
         list* sample = make_list();
         while((s = strtok(ptr, "\t")) != NULL){ // 以`Tab(\t)`分割字符串
-
-            if (n<2) {                          // 前两个字符串,依次保存
-                list_insert(sample, (void*)s);
-            } else if (n==2) {                  // 第三个字符串
-                if (strlen(s)>2) {              // 即不为数字,为人名,负样本
-                    list_insert(sample, (void*)s);
-                    list_insert(sample, (void*)strtok(ptr, "\t"));
-                } else {                        // 为数字,正样本
-                    list_insert(sample, (void*)sample->front->val);
-                    list_insert(sample, (void*)s);
-                }
-            }
-            n++; ptr = NULL;
+            list_insert(sample, (void*)s);
+            ptr = NULL;
         }
+
         list_insert(pairs, (void*)sample);
     }
     fclose(fp);
@@ -49,6 +40,7 @@ list* get_lfw_pairs()
 int verify_lfw_images(int argc, char** argv)
 {
     // ======================== INITIALIZE ======================== //
+    FILE* fp = fopen("bad_samples.txt", "w");
     params p = initParams(argc, argv);
     network* pnet = load_mtcnn_net("PNet");
     network* rnet = load_mtcnn_net("RNet");
@@ -59,40 +51,100 @@ int verify_lfw_images(int argc, char** argv)
 
     // ========================= LODA DATA ======================== //
     printf("Reading LFW...");
+    int bad_samples = 0, correct_samples = 0;
     list* pairs = get_lfw_pairs();
     printf("OK\n");
 
     // ======================= VERIFICATION ======================= //
-    int size = pairs->size;
     double time = what_time_is_it_now();
     double elapsed = 0;
-    for (int i = 0; i < pairs->size; i++ ){
+    int total_samples = pairs->size;
+    for (int i = 0; i < total_samples; i++ ){
         
         double duration = what_time_is_it_now() - time; 
         elapsed += duration; time = what_time_is_it_now();
         printf("\033[2J"); printf("\033[1;1H");
         printf("Pair [%4d]/[%4d] >> Elapsed %6.1fs\n",
-                        i+1, size, elapsed);
+                        i+1, total_samples, elapsed);
 
-        if (i == 494){
-            // TODO
-        }
         // --------------- GET A PAIR OF SAMPLE ------------------- //
         list* sample = (list*)list_pop(pairs);
-
-        int idx2 = atoi((char*)list_pop(sample));
-        char* name2 = (char*)list_pop(sample);
-        int idx1 = atoi((char*)list_pop(sample));
-        char* name1 = (char*)list_pop(sample);
+        char* name1 = NULL; char* name2 = NULL; 
+        int idx1 = -1, idx2 = -1;
+        if (sample->size == 3){
+            idx2 = atoi((char*)list_pop(sample));
+            idx1 = atoi((char*)list_pop(sample));
+            name2 = (char*)list_pop(sample);
+            name1 = (char*)name2;
+        } else {
+            idx2 = atoi((char*)list_pop(sample));
+            name2 = (char*)list_pop(sample);
+            idx1 = atoi((char*)list_pop(sample));
+            name1 = (char*)list_pop(sample);
+        }
         char buff[256];
         sprintf(buff, "%s/%s/%s_%04d.jpg", LFW_PATH, name1, name1, idx1);
-        image image1 = load_image_color(buff, 0, 0);
+        image im1 = load_image_color(buff, 0, 0);
         sprintf(buff, "%s/%s/%s_%04d.jpg", LFW_PATH, name2, name2, idx2);
-        image image2 = load_image_color(buff, 0, 0);
-        
-        // show_image(image1, "image1", 100); show_image(image2, "image2", 0);
+        image im2 = load_image_color(buff, 0, 0);
+        // show_image(im1, "im1", 10); show_image(im2, "im2", 10);
 
-        // if (0 == strcmp(name1, name2))
+        // ------------------- DETECT FACE ------------------------ //
+        int idx = 0;
+        // detect im1
+        detect* dets = calloc(0, sizeof(dets)); int n = 0;
+        detect_image(pnet, rnet, onet, im1, &n, &dets, p);
+        if (n == 0){
+            bad_samples += 1; 
+            fprintf(fp, "detected error: %d\n", i);
+            printf("Image1 is not detected!\n"); 
+            continue;
+        }
+        idx = keep_one(dets, n, im1); 
+        bbox box1 = dets[idx].bx; landmark landmark1 = dets[idx].mk;
+
+        // detect im2
+        dets = realloc(dets, 0); n = 0;
+        detect_image(pnet, rnet, onet, im2, &n, &dets, p);
+        if (n == 0){
+            bad_samples += 1; 
+            fprintf(fp, "detected error: %d\n", i);
+            printf("Image2 is not detected!\n"); 
+            continue;
+        }
+        idx = keep_one(dets, n, im2); 
+        bbox box2 = dets[idx].bx; landmark landmark2 = dets[idx].mk;
+
+        // ------------------- VERIFICATION ----------------------- //
+        image crop1 = crop_image_by_box(im1, box1, H, W);
+        image crop2 = crop_image_by_box(im2, box2, H, W);
+        image warped1 = align_image_with_landmark(crop1, landmark1, aligned);
+        image warped2 = align_image_with_landmark(crop2, landmark2, aligned);
+        // show_image(warped1, "warped1", 10); show_image(warped2, "warped2", 0);
+
+        int pred = verify(mobilefacenet, crop1, crop2, 0.4);    // if matched, pred = 1, else 0
+        int gt   = (0 == strcmp(name1, name2));                 // if mathced, gt = 1, else 0
+        if (pred == gt) correct_samples += 1;
+        else {
+            fprintf(fp, "verify error: %d\n", i);
+        }
+        printf("Gt: %d  Pred: %d\n", gt, pred);
+
+        free(name1);    // free line(fgetl)
+        free(dets);
+        free_image(im1); free_image(im2); 
+        free_image(crop1); free_image(crop2);
+        free_image(warped1); free_image(warped2);
+        
+        sleep(1);
     }
+    
+    // ========================= STATISTIC ======================== //
+    float correct_rate = (float)correct_samples / (float)(total_samples - bad_samples);
+    printf("\033[2J"); printf("\033[1;1H");
+    printf("Total: %4d  Bad: %4d  Correct: %4d  Rate: %2.2f\n", 
+            total_samples, bad_samples, correct_samples, correct_rate);
+    fclose(fp);
+
     return 0;
 }
