6d540dd30c3cd8c2514249afa08af400b218b77d
louishsu <is.louishsu@foxmail.com>
2019-06-03 10:15:37 +0800

    train

--- pretrained/train/config.py
+++ pretrained/train/config.py
@@ -6,11 +6,11 @@ configer.ckptdir = './ckpt'
 configer.logdir = './log'
 
 configer.inputsize = (3, 112, 96)    # (C, H, W)
-configer.batchsize = 128
-configer.n_epoch = 30
+configer.batchsize = 96
+configer.n_epoch = 40
 
-configer.lrbase = 0.001
-configer.adjstep = [12,]
+configer.lrbase = 0.0004
+configer.adjstep = [30,]
 configer.gamma = 0.1
 
 configer.cuda = True
