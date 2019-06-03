9ec7bdb592a4ae97ced65b9a5cf039de4d5ff210
louishsu <is.louishsu@foxmail.com>
2019-06-03 17:27:18 +0800

    c

--- pretrained/train/metrics.py
+++ pretrained/train/metrics.py
@@ -14,21 +14,32 @@ class ArcMarginProduct(nn.Module):
         self.cos_m = math.cos(m)
         self.sin_m = math.sin(m)
         # make the function cos(theta+m) monotonic decreasing while theta in [0°,180°]
-        self.th = math.cos(math.pi - m)
-        self.mm = math.sin(math.pi - m) * m
+        self.th = math.cos(math.pi - m)     # self.th = - math.cos(m)
+        self.mm = math.sin(math.pi - m) * m # self.mm =   math.sin(m) * m
 
     def forward(self, cosine, label):
-
+        """
+        Params:
+            cosine: {tensor(N, n_classes)} 每个样本(N)，到各类别(n_classes)矢量的余弦值
+            label:  {tensor(N)}
+        Returns:
+            output: {tensor(N, n_classes)}
+        Notes:
+            - L^{(i)} = - \log \frac{e^{s \cos(\theta_{y_i} + m)}}
+                                    {e^{s \cos(\theta_{y_i} + m)} + \sum_{j \neq y_i} e^{s \cos \theta_j}}
+            - \cos(\theta_{y_i} + m)} = \cos \theta_{y_i} \cos m - \sin \theta_{y_i} \sin m
+        """
         sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
-        phi = cosine * self.cos_m - sine * self.sin_m
+        phi = cosine * self.cos_m - sine * self.sin_m                           #    phi = cos(cosine + m)
         if self.easy_margin:
-            phi = torch.where(cosine > 0, phi, cosine)
+            phi = torch.where(cosine > 0, phi, cosine)                          # cosine = cos(cosine + m)      if cosine > 0
         else:
-            phi = torch.where((cosine - self.th) > 0, phi, cosine - self.mm)
+            phi = torch.where((cosine - self.th) > 0, phi, cosine - self.mm)    # cosine = cos(cosine + m)      if cosine > cos(m) else
+                                                                                # cosine = cosine - m * sin(m)
 
         one_hot = torch.zeros(cosine.size(), device='cuda' if torch.cuda.is_available() else 'cpu')
         one_hot.scatter_(1, label.view(-1, 1).long(), 1)
-        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)                   # cosine[y_i, i] = phi
         output = self.s * output
         return output
 
@@ -47,4 +58,18 @@ class MobileFacenetLoss(nn.Module):
         loss = self.classifier(output, gt)
         return loss
 
-        
+
+class MobileFacenetClusterLoss(nn.Module):
+
+    def __init__(self):
+        ...
+    
+    def forward(self, pred, gt):
+        """
+        Params:
+            pred: {tensor(N, n_features(128))}
+            gt:   {tensor(N)}
+        Returns:
+            loss: {tensor(1)}
+        """
+        ...
--- pretrained/train/trainer.py
+++ pretrained/train/trainer.py
@@ -122,6 +122,24 @@ def summary(model, input_size, batch_size=-1, device="cuda"):
     print("----------------------------------------------------------------")
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Trainer(object):
     """ Train Templet
     """
@@ -194,6 +212,189 @@ class Trainer(object):
         for i_epoch in range(n_epoch):
             
             if self.configer.cuda and cuda.is_available(): cuda.empty_cache()
+
+            self.cur_epoch += 1
+            bar.step()
+
+            self.lr_scheduler.step(self.cur_epoch)
+            cur_lr = self.lr_scheduler.get_lr()[-1]
+            self.writer.add_scalar('{}/lr'.format(self.net._get_name()), cur_lr, self.cur_epoch)
+
+            loss_train = self.train_epoch()
+            # print("----------------------------------------------------------------------------------------------")
+            loss_valid = self.valid_epoch()
+            # print("----------------------------------------------------------------------------------------------")
+
+            self.writer.add_scalars('loss', {'train': loss_train, 'valid': loss_valid}, self.cur_epoch)
+
+            # print_log = "{} || Elapsed: {:.4f}h || Epoch: [{:3d}]/[{:3d}] || lr: {:.6f},| train loss: {:4.4f}, valid loss: {:4.4f}".\
+            #         format(getTime(), self.elapsed_time/3600, self.cur_epoch, self.configer.n_epoch, 
+            #             cur_lr, loss_train, loss_valid)
+            # print(print_log)
+
+            if loss_valid < self.valid_loss:
+                self.valid_loss = loss_valid
+                self.save_checkpoint()
+                
+            # print("==============================================================================================")
+
+
+    def train_epoch(self):
+        
+        self.net.train()
+        avg_loss = []
+        start_time = time.time()
+        n_batch = len(self.trainset) // self.configer.batchsize
+
+        for i_batch, (X, y) in enumerate(self.trainloader):
+
+            self.cur_batch += 1
+
+            X = Variable(X.float()); y = Variable(y.long())
+            if self.configer.cuda and cuda.is_available(): X = X.cuda(); y = y.cuda()
+            
+            y_pred = self.net(X)
+            loss_i = self.criterion(y_pred, y)
+
+            self.optimizer.zero_grad()
+            loss_i.backward()
+            self.optimizer.step()
+
+            avg_loss += [loss_i.detach().cpu().numpy()]
+            self.writer.add_scalar('{}/train/loss_i'.format(self.net._get_name()), loss_i, self.cur_epoch*n_batch + i_batch)
+
+            duration_time = time.time() - start_time
+            start_time = time.time()
+            self.elapsed_time += duration_time
+            total_time = duration_time * self.configer.n_epoch * len(self.trainset) // self.configer.batchsize
+            left_time = total_time - self.elapsed_time
+
+            # print_log = "{} || Elapsed: {:.4f}h | Left: {:.4f}h | FPS: {:4.2f} || Epoch: [{:3d}]/[{:3d}] | Batch: [{:3d}]/[{:3d}] | cur: [{:3d}] || lr: {:.6f}, loss: {:4.4f}".\
+            #     format(getTime(), self.elapsed_time/3600, left_time/3600, self.configer.batchsize / duration_time,
+            #         self.cur_epoch, self.configer.n_epoch, i_batch, n_batch, self.cur_batch,
+            #         self.lr_scheduler.get_lr()[-1], loss_i
+            #     )
+            # print(print_log)
+        
+        avg_loss = np.mean(np.array(avg_loss))
+        return avg_loss
+
+
+    def valid_epoch(self):
+        
+        self.net.eval()
+        avg_loss = []
+        start_time = time.time()
+        n_batch = len(self.validset) // self.configer.batchsize
+
+        for i_batch, (X, y) in enumerate(self.validloader):
+
+            X = Variable(X.float()); y = Variable(y.long())
+            if self.configer.cuda and cuda.is_available(): X = X.cuda(); y = y.cuda()
+            
+            y_pred = self.net(X)
+            loss_i = self.criterion(y_pred, y)
+
+            avg_loss += [loss_i.detach().cpu().numpy()]
+            self.writer.add_scalar('{}/valid/loss_i'.format(self.net._get_name()), loss_i, self.cur_epoch*n_batch + i_batch)
+
+            duration_time = time.time() - start_time
+            start_time = time.time()
+
+            # print_log = "{} || FPS: {:4.2f} || Epoch: [{:3d}]/[{:3d}] | Batch: [{:3d}]/[{:3d}] || loss: {:4.4f}".\
+            #     format(getTime(), self.configer.batchsize / duration_time,
+            #         self.cur_epoch, self.configer.n_epoch, i_batch, n_batch, loss_i
+            #     )
+            # print(print_log)
+        
+        avg_loss = np.mean(np.array(avg_loss))
+        return avg_loss
+    
+    @classmethod
+    def save_checkpoint(self):
+        
+        checkpoint_state = {
+            'save_time': getTime(),
+
+            'cur_epoch': self.cur_epoch,
+            'cur_batch': self.cur_batch,
+            'elapsed_time': self.elapsed_time,
+            'valid_loss': self.valid_loss,
+            'save_times': self.save_times,
+            
+            'net': self.net.state_dict(),
+            'optimizer_state': self.optimizer.state_dict(),
+            'lr_scheduler_state': self.lr_scheduler.state_dict(),
+        }
+
+        checkpoint_path = os.path.join(self.ckptdir, "{}_{:04d}.pkl".\
+                            format(self.net._get_name(), self.save_times))
+        torch.save(checkpoint_state, checkpoint_path)
+        
+        checkpoint_path = os.path.join(self.ckptdir, "{}_{:04d}.pkl".\
+                            format(self.net._get_name(), self.save_times-self.num_to_keep))
+        if os.path.exists(checkpoint_path): os.remove(checkpoint_path)
+
+        self.save_times += 1
+
+        # print("checkpoint saved at {}".format(checkpoint_path))
+
+    @classmethod
+    def load_checkpoint(self, index):
+        
+        checkpoint_path = os.path.join(self.ckptdir, "{}_{:04d}.pkl".\
+                            format(self.net._get_name(), index))
+        checkpoint_state = torch.load(checkpoint_path, map_location='cuda' if cuda.is_available() else 'cpu')
+        
+        self.cur_epoch = checkpoint_state['cur_epoch']
+        self.cur_batch = checkpoint_state['cur_batch']
+        self.elapsed_time = checkpoint_state['elapsed_time']
+        self.valid_loss = checkpoint_state['valid_loss']
+        self.save_times = checkpoint_state['save_times']
+
+        self.net.load_state_dict(checkpoint_state['net'])
+        self.optimizer.load_state_dict(checkpoint_state['optimizer_state'])
+        self.lr_scheduler.load_state_dict(checkpoint_state['lr_scheduler_state'])
+
+        # print("load checkpoint from {}, last save time: {}".\
+        #                         format(checkpoint_path, checkpoint_state['save_time']))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class TrainerCluster(Trainer):
+
+    def __init__(self, configer, net, params, trainset, validset, criterion, optimizer, lr_scheduler, num_to_keep=5, resume=False):
+
+        super(TrainerCluster, self).__init__(configer, net, params, trainset, validset, 
+                        criterion, optimizer, lr_scheduler, num_to_keep, resume)
+
+    def train(self):
+        
+        n_epoch = self.configer.n_epoch - self.cur_epoch
+        print("Start training! current epoch: {}, remain epoch: {}".format(self.cur_epoch, n_epoch))
+
+        bar = ProcessBar(n_epoch)
+
+        for i_epoch in range(n_epoch):
+            
+            if self.configer.cuda and cuda.is_available(): 
+                cuda.empty_cache()
 
             self.cur_epoch += 1
             bar.step()
