de860a8a77c3b673fa2b70a5eb931f0035494ba0
louishsu <is.louishsu@foxmail.com>
2019-06-10 11:36:04 +0800

    unsupervised

--- pretrained/train/config.py
+++ pretrained/train/config.py
@@ -7,11 +7,11 @@ configer.logdir = './log'
 
 configer.inputsize = (3, 112, 96)    # (C, H, W)
 configer.batchsize = 128
-configer.n_epoch = 70
+configer.n_epoch = 50
 configer.valid_freq = 0
 
 configer.lrbase = 0.001
-configer.adjstep = [36, 52, 58]
+configer.adjstep = [32, 44]
 configer.gamma = 0.1
 
 configer.cuda = True
