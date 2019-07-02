da5f9efb58032017751169d0eef5d43b5ada2c43
louishsu <is.louishsu@foxmail.com>
2019-07-02 17:49:34 +0800

    src

--- src/mtcnn.c
+++ src/mtcnn.c
@@ -66,7 +66,7 @@ params initParams(int argc, char **argv)
 {
     params p;
 
-    p.min_face = find_float_arg(argc, argv, "--minface", 24.0);
+    p.min_face = find_float_arg(argc, argv, "--minface", 196.0);
     p.pthresh  = find_float_arg(argc, argv, "-p", 0.8);
     p.rthresh  = find_float_arg(argc, argv, "-r", 0.8);
     p.othresh  = find_float_arg(argc, argv, "-o", 0.8);
